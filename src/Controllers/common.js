f5102126c6ebc003d138ac30beb4784cd8f7b348
Developer-Zaid <zk145430@gmail.com>
2026-02-14 17:29:31 +0530

    ok alhamdulillah changes completed

--- src/Controllers/common.js
+++ src/Controllers/common.js
@@ -5,8 +5,10 @@ const clientPromise = require("../lib/mongodb");
 const Contactform = async (req, res) => {
   try {
     if (!req.body.verifypass || req.body.verifypass !== process.env.VERIFY_PASS) {
+      console.log("password galat hai")
       return res.status(502).json({ error: "SOME THING WENT WRONG" })
     }
+    console.log("password sahi hai")
     const client = await clientPromise;
     const db = client.db("developerzaid");
     const today = new Date();
@@ -17,8 +19,10 @@ const Contactform = async (req, res) => {
         date: formattedDate,
         name: req.body.name,
         contactdetails: req.body.contactinfo,
-        message: req.body.mesage
+        message: req.body.message
       }
+      console.log("data aya hai")
+      console.log(info)
       const result = await db.collection("ContactForms").insertOne(info);
       res.json({ success: true, result });
 
