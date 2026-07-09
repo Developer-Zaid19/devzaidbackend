f3d91e9a89a39c3eb31c08b3e21a2aa265ce06ff
Developer-Zaid <zk145430@gmail.com>
2026-07-10 00:08:30 +0530

    complete alhamdulillah

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const cors = require("cors");
 const clientPromise = require("./src/lib/mongodb.js");
 const contentrouter = require("./src/Routes/contentwebandapp.js");
@@ -16,10 +17,41 @@ app.use(
   express.static(path.join(__dirname, "src/Public"))
 );
 
-app.use(
-  "/downloads",
-  express.static(path.join(process.cwd(), "src", "temp"))
-);
+// app.use(
+//   "/downloads",
+//   express.static(path.join(process.cwd(), "src", "temp"))
+// );
+
+app.get("/downloads/:filename", (req, res) => {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "temp",
+    req.params.filename
+  );
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({
+      success: false,
+      message: "File not found",
+    });
+  }
+
+  res.download(filePath, req.params.filename, err => {
+    if (err) {
+      console.error("Download Error:", err);
+      return;
+    }
+
+    fs.unlink(filePath, unlinkErr => {
+      if (unlinkErr) {
+        console.error("Delete Error:", unlinkErr);
+      } else {
+        console.log("Deleted:", req.params.filename);
+      }
+    });
+  });
+});
 
 console.log("server pe request giri sir !")
 app.get("/", (req, res) => {
